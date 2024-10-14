8c2a283a1709136ffd9be2f893f74672d9049ca9
jorge <jorge@INSPIRON153511>
2024-10-14 11:05:25 -0600

    Se creo el SeedDB para crear y poblar la base de datos y tablas

---
+++ Orders.Backend/Data/SeedDB.cs
@@ -0,0 +1,51 @@
+﻿using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class SeedDB
+    {
+        //Propiedad privada para usarla en toda la sollucion
+        private readonly DataContext _context;
+
+        //Inyectamos el contexto para acceder a la base de datos
+        public SeedDB(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            //Asegurarse de que hay Base de datos, crea un update-database por codigo
+            await _context.Database.EnsureCreatedAsync();
+
+            //Metodo para crear paises
+            await CheckCountriesAsync();
+            //Metodo para crear categorias
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            //Si no hay ninguna categoria
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Deportes" });
+                _context.Categories.Add(new Category { Name = "Hogar" });
+                _context.Categories.Add(new Category { Name = "Jardin" });
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            //Si no hay ningun pais
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country { Name = "Guatemala" });
+                _context.Countries.Add(new Country { Name = "Honduras" });
+                _context.Countries.Add(new Country { Name = "Nicaragua" });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Orders.Backend/Program.cs
+++ Orders.Backend/Program.cs
@@ -17,11 +17,27 @@ builder.Services.AddSwaggerGen();
 //Inyectamos el servicio para conectarse al SqlServer
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=CadenaSql"));
 
-//Inyectamos Los genericos del Repositorio y del UnitOfWork
+//Inyectamos el servicio de alimentador de base de datos, usamos Trasient porque solo se usa uno sola vez al cargar la aplicacion y por eso la colocamos en area de momoria no inmeditata
+builder.Services.AddTransient<SeedDB>();
+
+//Inyectamos Los genericos del Repositorio y del UnitOfWork, usamos Scoped porque se utilizan muchas veces en la aplicacion y la ponemos en un area de memoria accesible y rapida
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 
 var app = builder.Build();
+
+//Para inyectar el SeedDB no se puede hacer directamente al program que es la clase que inyecta
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory!.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedDB>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
