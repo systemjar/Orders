21162616b2cc2120a98a3a27cebb70673b1b9a4c
jorge <jorge@INSPIRON153511>
2024-10-30 14:12:18 -0600

    Login, Logout y Register v1

--- Orders.Frontend/Program.cs
+++ Orders.Frontend/Program.cs
@@ -5,12 +5,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
 using Orders.Frontend.AuthenticationProviders;
 using Orders.Frontend.Repositories;
+using Orders.Frontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //Esta es la url que provee los servicios del backend
+//Utilizamos AddSingleton para que solo haya um HttpCliente por toda la ejecucion del programa
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7192/") });
 
 //Inyectamos el Repositorio porque el Frontend solo obtiene datos de repositorio
@@ -23,6 +25,14 @@ builder.Services.AddSweetAlert2();
 builder.Services.AddAuthorizationCore();
 
 //Proveedor de seguridad
-builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+/* Linea anterior
+ * builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();*/
+
+//Se reemplaza por esto
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x =>
+x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x =>
+x.GetRequiredService<AuthenticationProviderJWT>());
 
 await builder.Build().RunAsync();
